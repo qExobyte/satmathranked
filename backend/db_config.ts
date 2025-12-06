e3c33ce4d205049c46ed745b4d80d9a597442a04
Sahlar7 <rmh7ce@virginia.edu>
2025-12-06 13:28:14 -0500

    database connection file

---
+++ backend/db_config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT)!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+export default pool;
