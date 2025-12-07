c5c0875e0a7af4a204ce503163877d3b938cd1b8
Sahlar7 <rmh7ce@virginia.edu>
2025-12-07 17:55:01 -0500

    fix sql query for existing user

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -50,7 +50,7 @@ router.get('/google/callback', async (req: Request, res: Response) => {
         }
 
         const [existingUsers] = await pool.query(
-            'SELECT ID, Username, Email_Address FROM USERS WHERE Email_Address = ?',
+            'SELECT ID as id, Username as username, Email_Address as email_address FROM USERS WHERE email_address = ?',
             [payload.email]
         );
 
@@ -78,9 +78,9 @@ router.get('/google/callback', async (req: Request, res: Response) => {
         // Change this to whatever info we actually want
         const userInfo = {
             //googleId: payload.sub,
-            id: user.ID,
-            email: user.Email_Address,
-            name: user.Username,
+            id: user.id,
+            email: user.email_address,
+            name: user.username,
             elo: elo
             //picture: payload.picture,
             //email_verified: payload.email_verified
